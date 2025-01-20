7af57a15ec4edee8b7f2ea30b2a3338f552370a7
languyen08 <thanhlan0813@gmail.com>
2025-01-21 00:38:48 +0700

    first commit

---
+++ auth/src/model/user.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import {Password} from "../service/password";
+
+interface UserAttributes {
+    email: string;
+    password: string;
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(userAttributes: UserAttributes): UserDoc;
+}
+
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        require: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+}, {
+    toJSON: {
+        transform(doc, ret) {
+            ret.id = ret._id;
+            delete ret._id;
+            delete ret.password;
+            delete ret.__v;
+        }
+    }
+});
+userSchema.pre('save', async function(done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password')!);
+        this.set('password', hashed);
+    }
+    done();
+});
+userSchema.statics.build = (userAttributes: UserAttributes) => {
+    return new User(userAttributes);
+};
+
+const User: UserModel = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export {User};
