b781feaabd691098692a5f9e03d4b7dae4d82777
languyen08 <thanhlan0813@gmail.com>
2025-02-03 00:54:07 +0700

    Learning from 278 to 291

--- auth/src/model/user.ts
+++ auth/src/model/user.ts
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 import { PasswordManager } from "../service/password-manager";
 
+// An interface that describes the properties that are required to create a new User
 interface UserAttributes {
     email: string;
     password: string;
 }
-
+// An interface that describes the properties that a User Model has
 interface UserModel extends mongoose.Model<UserDoc> {
     build(userAttributes: UserAttributes): UserDoc;
 }
-
+// An interface that describes the properties that a User Document has
 interface UserDoc extends mongoose.Document {
     email: string;
     password: string;
--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,6 +3,9 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler, NotFoundError } from "@ntlantickets/common";
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
 
 const app: Express = express();
 app.set('trust proxy', true);
@@ -15,6 +18,10 @@ app.use(
 )
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
 app.all('*', async () => {
     throw new NotFoundError();
 })
